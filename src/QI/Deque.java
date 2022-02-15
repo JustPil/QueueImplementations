8939fa9d5979fa80b8fc8d797281885c565de24b
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 20:58:56 -0800

    Minor style updates

--- src/QI/Deque.java
+++ src/QI/Deque.java
@@ -1,7 +1,6 @@
 package QI;
 
-public class Deque<T> implements DequeInterface<T>
-{
+public class Deque<T> implements DequeInterface<T> {
     private final int DEFAULT = 100;
     private T[] items;
     private int totalItems;
@@ -9,39 +8,36 @@ public class Deque<T> implements DequeInterface<T>
     private int rear;
 
     /**
-     * Constructor instantiates the Deque's array to the default and sets the rear to the last index of the array.
+     * Constructor instantiates the Deque to default capacity.
      */
-    public Deque()
-    {
+    public Deque() {
         items = (T[])new Object[DEFAULT];
         rear = DEFAULT - 1;
     }
 
     /**
-     * Constructor instantiates the Deque's array to the size passed in and sets the rear to the last index of the
-     * array.
-     * @param arraySize The size of the array.
+     * Constructor instantiates the Deque to a user-specified capacity
+     * @param arraySize The size of the Deque.
      */
-    public Deque(int arraySize)
-    {
-        if(arraySize < 0)
-            throw new EmptyQueueDequeueException("Array cannot have less than 0 indexes.");
+    public Deque(int arraySize) {
+        if(arraySize < 0) {
+            throw new EmptyQueueDequeueException("A Deque cannot have negative capacity");
+        }
         items = (T[])new Object[arraySize];
         rear = arraySize - 1;
     }
 
     /**
-     * dequeueFront Removes and returns the front value in the array.
+     * dequeueFront Removes and returns the front value in the Deque.
      * @return The value to be removed.
      */
-    public T dequeueFront()
-    {
-        if(isEmpty())
+    public T dequeueFront() {
+        if(isEmpty()) {
             throw new EmptyQueueDequeueException("dequeue attempted on an empty queue.");
+        }
         T temp = items[0];
         T[] tempArray = (T[])new Object[items.length];
-        for(int i = 0; i < totalItems - 1; i++)
-        {
+        for(int i = 0; i < totalItems - 1; i++) {
             tempArray[i] = items[i + 1];
         }
         items = tempArray;
@@ -51,13 +47,13 @@ public class Deque<T> implements DequeInterface<T>
     }
 
     /**
-     * dequeueRear Removes and returns the rear value in the array.
+     * dequeueRear Removes and returns the rear value in the Deque.
      * @return The value to be removed.
      */
-    public T dequeueRear()
-    {
-        if(isEmpty())
+    public T dequeueRear() {
+        if(isEmpty()) {
             throw new EmptyQueueDequeueException("dequeue attempted on an empty queue.");
+        }
         T temp = items[rear];
         rear = (rear - 1) % items.length;
         totalItems--;
@@ -65,13 +61,13 @@ public class Deque<T> implements DequeInterface<T>
     }
 
     /**
-     * enqueueFront Adds a value to the front of the array.
+     * enqueueFront Adds a value to the front of the Deque.
      * @param input The value to be inserted.
      */
-    public void enqueueFront(T input)
-    {
-        if(isFull())
+    public void enqueueFront(T input) {
+        if(isFull()) {
             throw new FullQueueEnqueueException("enqueue attempted on a full queue.");
+        }
         T[] tempArray = (T[])new Object[items.length];
         tempArray[0] = input;
         for(int i = 0; i < totalItems; i++)
@@ -84,42 +80,39 @@ public class Deque<T> implements DequeInterface<T>
     }
 
     /**
-     * enqueueRear Adds a value to the rear of the array.
+     * enqueueRear Adds a value to the rear of the Deque.
      * @param input The value to be inserted.
      */
-    public void enqueueRear(T input)
-    {
-        if(isFull())
+    public void enqueueRear(T input) {
+        if(isFull()) {
             throw new FullQueueEnqueueException("enqueue attempted on a full queue.");
+        }
         rear = (rear + 1) % items.length;
         items[rear] = input;
         totalItems++;
     }
 
     /**
-     * isEmpty Checks if there are no items in the deque.
-     * @return True if there are no items in the deque, false otherwise.
+     * isEmpty Checks if there are no items in the Deque.
+     * @return True if there are no items in the Deque, false otherwise.
      */
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return totalItems == 0;
     }
 
     /**
-     * Checks if there are maximum items in the deque.
-     * @return True if there are maximum items in the deque, false otherwise.
+     * Checks if there are maximum items in the Deque.
+     * @return True if there are maximum items in the Deque, false otherwise.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return totalItems == items.length;
     }
 
     /**
-     * size Returns the total items in the deque.
-     * @return The number of items in the deque.
+     * size Returns the total items in the Deque.
+     * @return The number of items in the Deque.
      */
-    public int size()
-    {
+    public int size() {
         return totalItems;
     }
 }
