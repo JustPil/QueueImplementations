5bc24819ac9106e36eac41d62da757732dab41be
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 21:06:06 -0800

    Minor style updates

--- src/QI/LinkedListDeque.java
+++ src/QI/LinkedListDeque.java
@@ -1,7 +1,6 @@
 package QI;
 
-public class LinkedListDeque<T> implements DequeInterface<T>
-{
+public class LinkedListDeque<T> implements DequeInterface<T> {
     private Node<T> front;
     private Node<T> rear;
     private int nodeCount = 0;
@@ -9,25 +8,20 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     /**
      * Constructor sets the front and rear nodes to null.
      */
-    public LinkedListDeque()
-    {
+    public LinkedListDeque() {
         front = rear = null;
     }
 
     /**
-     * enqueueFront Inserts a new node at the front of the linked list.
+     * enqueueFront Inserts a new node at the front of the Deque.
      * @param data The data for the new node to hold.
      */
-    public void enqueueFront(T data)
-    {
+    public void enqueueFront(T data) {
         Node<T> insert = new Node<>(data);
-        if(nodeCount == 0)
-        {
+        if(nodeCount == 0) {
             front = rear = insert;
             nodeCount++;
-        }
-        else
-        {
+        } else {
             insert.setNext(front);
             front = insert;
             nodeCount++;
@@ -35,19 +29,15 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     }
 
     /**
-     * enqueueRear Inserts a new node at the rear of the linked list.
+     * enqueueRear Inserts a new node at the rear of the Deque.
      * @param data The data for the new node to hold.
      */
-    public void enqueueRear(T data)
-    {
+    public void enqueueRear(T data) {
         Node<T> insert = new Node<>(data);
-        if(nodeCount == 0)
-        {
+        if(nodeCount == 0) {
             front = rear = insert;
             nodeCount++;
-        }
-        else
-        {
+        } else {
             rear.setNext(insert);
             rear = insert;
             nodeCount++;
@@ -55,21 +45,18 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     }
 
     /**
-     * dequeueFront Removes a node from the front of the linked list.
+     * dequeueFront Removes a node from the front of the Deque.
      * @return The data held by the removed node.
      */
-    public T dequeueFront()
-    {
-        if(isEmpty())
+    public T dequeueFront() {
+        if(isEmpty()) {
             throw new EmptyQueueDequeueException("dequeue attempted on an empty queue.");
+        }
         T removedNode = front.getData();
-        if(nodeCount == 1)
-        {
+        if(nodeCount == 1) {
             front = rear = null;
             nodeCount--;
-        }
-        else
-        {
+        } else {
             front = front.getNext();
             nodeCount--;
         }
@@ -77,26 +64,21 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     }
 
     /**
-     * dequeueRear Removes a node from the rear of the linked list.
+     * dequeueRear Removes a node from the rear of the Deque.
      * @return The data held by the removed node.
      */
-    public T dequeueRear()
-    {
-        if(isEmpty())
+    public T dequeueRear() {
+        if(isEmpty()) {
             throw new EmptyQueueDequeueException("dequeue attempted on an empty queue.");
+        }
         T removedNode = rear.getData();
-        if(nodeCount == 1)
-        {
+        if(nodeCount == 1) {
             front = rear = null;
             nodeCount--;
-        }
-        else
-        {
+        } else {
             Node<T> parser = front;
-            while(parser != null)
-            {
-                if(parser.getNext().getNext() == null)
-                {
+            while(parser != null) {
+                if(parser.getNext().getNext() == null) {
                     parser.setNext(null);
                     nodeCount--;
                 }
@@ -107,79 +89,69 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     }
 
     /**
-     * isEmpty Checks if there are no nodes in the linked list.
-     * @return True if there are no nodes, false otherwise.
+     * isEmpty Checks if the Deque is empty..
+     * @return True if the Deque is empty, false otherwise.
      */
-    public boolean isEmpty()
-    {
-        return (nodeCount == 0);
+    public boolean isEmpty() {
+        return nodeCount == 0;
     }
 
     /**
-     * isFull Checks if the linked list is full.
-     * @return False because a linked list cannot be full.
+     * isFull Checks if the Deque is full.
+     * @return False, as a Deque implemented with a linked list cannot be full.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return false;
     }
 
     /**
-     * size Returns the size of the linked list.
-     * @return The amount of nodes in the linked list.
+     * size Returns the size of the Deque.
+     * @return The amount of nodes in the Deque.
      */
-    public int size()
-    {
+    public int size() {
         return nodeCount;
     }
 
     /**
-     * toString Outputs information about the current contents of the linked list.
-     * @return A string containing information about the current contents of the linked list.
+     * toString Outputs information about the Deque.
+     * @return A string containing information about the Deque.
      */
-    public String toString()
-    {
+    public String toString() {
         return "Front: " + front.getData() + "\nRear: " + rear.getData() + "\nSize: " + nodeCount + "\nEmpty: " +
                 isEmpty() + "\nFull: " + isFull();
     }
 
     /**
-     * remove Removes a set amount of nodes from the front of the linked list.
+     * remove Removes a set amount of nodes from the front of the Deque.
      * @param count The amount of nodes to be removed.
      */
-    public void remove(int count)
-    {
-        if(count > nodeCount)
+    public void remove(int count) {
+        if(count > nodeCount) {
             throw new EmptyQueueDequeueException("Not enough elements for removal.");
-        for(int i = 0; i < count; i++)
-        {
+        }
+        for(int i = 0; i < count; i++) {
             front = front.getNext();
             nodeCount--;
-            if(nodeCount == 0)
-            {
+            if(nodeCount == 0) {
                 front = rear = null;
             }
         }
     }
 
     /**
-     * swapStart Swaps the first two nodes at the front of the linked list.
+     * swapStart Swaps the first two nodes at the front of the Deque.
      * @return True if the swap was successful, false otherwise.
      */
-    public boolean swapStart()
-    {
-        if(nodeCount < 2)
+    public boolean swapStart() {
+        if(nodeCount < 2) {
             return false;
-        else if(nodeCount == 2)
-        {
+        } else if(nodeCount == 2) {
             rear.setNext(front);
             front.setNext(null);
             front = rear;
             rear = front.getNext();
             System.out.println();
-        }
-        else
-        {
+        } else {
             Node<T> temp1 = front.getNext();
             Node<T> temp2 = temp1.getNext();
             temp1.setNext(front);
@@ -190,15 +162,14 @@ public class LinkedListDeque<T> implements DequeInterface<T>
     }
 
     /**
-     * swapEnds Swqaps the last two nodes at the rear of the linked list.
+     * swapEnds Swaps the last two nodes at the rear of the Deque.
      * @return True if the swap was successful, false otherwise.
      */
-    public boolean swapEnds()
-    {
-        if(nodeCount < 2)
+    public boolean swapEnds() {
+        if(nodeCount < 2) {
             return false;
-        if(nodeCount == 2)
-        {
+        }
+        if(nodeCount == 2) {
             front.setNext(null);
             rear.setNext(front);
             front = rear;
@@ -206,10 +177,8 @@ public class LinkedListDeque<T> implements DequeInterface<T>
             return true;
         }
         Node<T> parser = front;
-        while(parser.getNext() != null)
-        {
-            if(parser.getNext().getNext().getNext() == null)
-            {
+        while(parser.getNext() != null) {
+            if(parser.getNext().getNext().getNext() == null) {
                 Node<T> temp = parser.getNext();
                 parser.setNext(rear);
                 rear.setNext(temp);
