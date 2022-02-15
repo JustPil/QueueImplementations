337054b7c7f509ac7bf1c2e8b304d916dd237a9c
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-14 21:22:13 -0800

    Minor style updates

--- src/QI/Node.java
+++ src/QI/Node.java
@@ -1,26 +1,23 @@
 package QI;
 
-public class Node<T>
-{
+public class Node<T> {
     private T data;
     private Node<T> next;
 
     /**
-     * Constrctor sets a node's data to the value passed in, and the next node to null.
+     * Constructor sets a node's data to the value passed in, and the next node to null.
      * @param t The value to be held by the node.
      */
-    public Node(T t)
-    {
+    public Node(T t) {
         data = t;
         next = null;
     }
 
     /**
      * setData Sets the node's data to the value passed in.
-     * @param t The data to be held bu the node.
+     * @param t The data to be held by the node.
      */
-    public void setData(T t)
-    {
+    public void setData(T t) {
         data = t;
     }
 
@@ -28,26 +25,23 @@ public class Node<T>
      * getData Returns the data held by the node.
      * @return The data held by the node.
      */
-    public T getData()
-    {
+    public T getData() {
         return data;
     }
 
     /**
-     * setNext Sets the next node in the linked list to the node passed in.
-     * @param n The next node in the linked list.
+     * setNext Sets the Node's next reference..
+     * @param n The next node reference.
      */
-    public void setNext(Node<T> n)
-    {
+    public void setNext(Node<T> n) {
         next = n;
     }
 
     /**
-     * getNext Returns the next node in the linked list.
-     * @return The next node in the linked list.
+     * getNext Returns the next node reference.
+     * @return The next node reference.
      */
-    public Node<T> getNext()
-    {
+    public Node<T> getNext() {
         return next;
     }
 }
