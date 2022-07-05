e832aec9ef43467e89e4484259e6d2ba090ab774
JustPil <88640664+JustPil@users.noreply.github.com>
2022-07-04 17:21:58 -0700

    Incoming changes to project structure

--- src/QI/QueueInterface.java
+++
@@ -1,9 +0,0 @@
-package QI;
-
-public interface QueueInterface<T> {
-    void enqueue(T element) throws FullQueueEnqueueException;
-    T dequeue() throws EmptyQueueDequeueException;
-    boolean isFull();
-    boolean isEmpty();
-    int size();
-}
