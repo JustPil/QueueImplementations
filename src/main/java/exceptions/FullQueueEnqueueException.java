110cffa4693ed9cce732d4871dc18e9472855492
justpil <88640664+JustPil@users.noreply.github.com>
2023-03-02 19:40:33 -0800

    Update project structure

---
+++ src/main/java/exceptions/FullQueueEnqueueException.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+public class FullQueueEnqueueException extends RuntimeException{
+    /**
+     * Constructor uses no error message.
+     */
+    public FullQueueEnqueueException() {
+        super();
+    }
+
+    /**
+     * Constructor specifies an error message.
+     * @param message The error message.
+     */
+    public FullQueueEnqueueException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/queuetests/ArrayListQueueTests.java
@@ -0,0 +1,56 @@
+package queuetests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
+import queues.ArrayListQueue;
+
+public class ArrayListQueueTests {
+    private final ArrayListQueue<Integer> queue = new ArrayListQueue<>();
+
+    @Test
+    public void enqueueAnElement() {
+        queue.enqueue(1);
+        var result = queue.size();
+        Assertions.assertEquals(1, result);
+    }
+
+    @Test
+    public void dequeueAnElement() {
+        queue.enqueue(1);
+        queue.dequeue();
+        var result = queue.isEmpty();
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void enqueueOnFullQueue() {
+        var result = queue.isFull();
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void dequeueOnEmptyQueue() {
+        Assertions.assertThrows(RuntimeException.class, queue::dequeue);
+    }
+
+    @Test
+    public void getElementsAsString() {
+        for(int i = 0; i < 3; i++) {
+            queue.enqueue(i + 1);
+        }
+        var result = queue.toString();
+        Assertions.assertEquals("[1 2 3]", result);
+    }
+
+    @Test
+    public void getElementsAsStringAfterDequeues() {
+        for(int i = 0; i < 10; i++) {
+            queue.enqueue(i + 1);
+        }
+        for(int i = 0; i < 5; i++) {
+            queue.dequeue();
+        }
+        var result = queue.toString();
+        Assertions.assertEquals("[6 7 8 9 10]", result);
+    }
+}
